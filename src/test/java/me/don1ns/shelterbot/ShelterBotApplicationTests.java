580e06b8633cff5d99f34cf1036444288afa28cc
Don1ns <riaz.karimullin@yandex.ru>
2023-04-07 20:54:31 +0300

    Initial commit

---
+++ src/test/java/me/don1ns/shelterbot/ShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.don1ns.shelterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
