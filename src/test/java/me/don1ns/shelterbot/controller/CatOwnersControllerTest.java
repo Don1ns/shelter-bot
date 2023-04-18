40992431b1fa19baa26d4ee2b1df44d378490db8
Jappananny <82016498+Jappananny@users.noreply.github.com>
2023-04-18 19:22:33 +0300

    Исправил: -pom -exception -внез правки по замечанием преподователя (только то что делал я) -немного тестиков (еще в процесе, сложно и непонятно)

--- src/test/java/me/don1ns/shelterbot/controller/CatOwnersControllerTest.java
+++ src/test/java/me/don1ns/shelterbot/controller/CatOwnersControllerTest.java
@@ -1,5 +1,8 @@
 package me.don1ns.shelterbot.controller;
-
+/**
+ * Класс тестов контролера для владельцев котиков
+ * @author Королёв Артем
+ **/
 public class CatOwnersControllerTest {
     //TODO Создать unitTest на класс CatOwnersController
 }
