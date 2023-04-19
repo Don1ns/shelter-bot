acfa45f062bea7ab6227bf66786e226cdd12089c
Elena Nikitina <elena_nikitina_86@inbox.ru>
2023-04-19 21:26:48 +0300

    Тесты для DogService.

---
+++ src/test/java/me/don1ns/shelterbot/controller/DogControllerTest.java
@@ -0,0 +1,17 @@
+package me.don1ns.shelterbot.controller;
+import org.junit.jupiter.api.Test;
+
+class DogControllerTest {
+
+    @Test
+    private void shouldAddDog() {
+    }
+
+    @Test
+    void shouldUpdateDogById() {
+    }
+
+    @Test
+    void shouldRemoveDog() {
+    }
+}
