a872343e0b1e69390479d36904a12d2c279d94cc
Riyaz Karimullin <60465203+Don1ns@users.noreply.github.com>
2023-04-20 12:52:21 +0300

    Revert "Feature jappananny"

--- src/main/java/me/don1ns/shelterbot/repository/DogOwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package me.don1ns.shelterbot.repository;
-
-import me.don1ns.shelterbot.model.DogOwner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DogOwnerRepository extends JpaRepository<DogOwner, Long> {
-}
